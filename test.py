cd8dc264ee656bb119ed6e192d9aefc862907199
Vincenzo Martemucci <vincenzomartemucci@protonmail.com>
2021-07-09 23:18:57 +0200

    libraries cleaning and merge

--- test.py
+++ test.py
@@ -1,59 +1,70 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import sklearn
-from sklearn.metrics import mean_squared_error
-
-from sklearn.preprocessing import RobustScaler
+from sklearn.linear_model import LassoCV
 from sklearn.preprocessing import StandardScaler
-
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import ElasticNet
-from sklearn.neighbors import KNeighborsRegressor
-
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 
-df = pd.read_csv('data/auto-mpg.csv')
+# from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error, mean_absolute_percentage_error
 
-pd.set_option('display.max_columns', None) # mostra tutte le colonne
+df = pd.read_csv('data/auto_mpg.csv')
 
-print(df.head())
-print(df.dtypes)
+pd.set_option('display.max_columns', None)  # show all the columns
+# print(df.describe())
+# print(df.head())
+# print(df.dtypes)
+
+list(df.columns)
+df.drop(labels=['car name'], axis=1, inplace=True)
 
 # Exploratory data Analysis visualization and analysis
 plt.figure(figsize=(10, 8))
 sns.histplot(df.mpg)
-plt.show()
-
-# searching for null values
-print(df.isnull().sum())
-
+# plt.show()
 
 # Correlation
-#f, ax = plt.subplots(figsize=[14, 8])
+# f, ax = plt.subplots(figsize=[14, 8])
 sns.heatmap(df.corr(), annot=True, fmt=".2f")
-#ax.set_title("Correlation Matrix", fontsize=20)
-plt.show()
+# ax.set_title("Correlation Matrix", fontsize=20)
+# plt.show()
 
 sns.pairplot(df, diag_kind='kde')
-plt.show()
+# plt.show()
 
-#plt.figure(figsize=[14, 6])
-#sns.barplot(x=df['model year'] + 1900, y=df['mpg'])
-#plt.title('Consumption Gallon by Years')
-#plt.show()
+plt.figure(figsize=[14, 6])
+sns.barplot(x=df['model year'] + 1900, y=df['mpg'])
+plt.title('Consumption Gallon by Years')
+# plt.show()
 
-print(df.corr('spearman'))
+# print(df.corr('spearman'))
 
-# train test split
-x = df.drop(["mpg"], axis=1)
-y = df.mpg
-print(x.head())
 
-test_size = 0.3
-X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=test_size, random_state=42)
+# df['acceleration_power_ratio'] = df['acceleration'] / df['horsepower']
 
+y = df['mpg']
+df.drop('mpg', axis=1, inplace=True)
+
+X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.3, random_state=3)
+model = Pipeline(steps=[('scaler', StandardScaler(),), ('lasso', LassoCV(),)])
+model.fit(X_train, y_train)
+prediction = model.predict(X_test)
+
+# cylinders,displacement,horsepower,weight,acceleration,model year,origin
+# input_data = (4,97.0,88.0,2130,14.5,70,3) #27
+# input_data = (8,318.0,140.0,3735,13.2,78,1) #19.4
+input_data = (4, 79.0, 58.0, 1755, 16.9, 81, 3)  # 39.1
+# input_data=(6,168.0,116.0,2900,12.6,81,3) #25.4
+# input_data=(8,351.0,266.0,2860,6,73,3) #de tomaso pantera 13.1-15.8 / 12.6
+
+
+# change the input data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the numpy array as we are predicting for only on instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+
+prediction = model.predict(input_data_reshaped)
+print(prediction)
