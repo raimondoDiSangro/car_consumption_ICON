ad82090a0e44f631d80c730d6198ebc86d1ebb24
Vincenzo Martemucci <vincenzomartemucci@protonmail.com>
2021-07-09 16:02:26 +0200

    added more data analysis, data exploration

--- README.md
+++ README.md
@@ -10,26 +10,14 @@
 - Considerata l'età dei dati, ho ritenuto opportuno aggiornare il dataset personalmente con dati di vetture moderne.
 
 
-### Contenuto dataset originale
-  1. ```mpg: consumo in miglia per gallone (miglio = 1.609 km circa, gallone = 3,785 litri)```
+### Contenuto dataset 
+  1. ```mpg: consumo in miglia per gallone in ambito cittadino(miglio = 1.609 km circa, gallone = 3,785 litri)```
   2. ```cylinders: numero di cilindri ```
   3. ```displacement: cilindrata in cu in (pollici cubo, 1 cu in = 16,387cc circa)```
   4. ```horsepower: potenza in cavalli```
   5.```weight: peso in libbre (1lb = 0,453 kg circa)```
   6. ```acceleration: 0-60mph espressa in secondi```
   7. ```model year: anno di produzione(dal 1970 al 1982)```
-  8. ```origin: area di origine (1- USA, 2-Europa, 3-Asia)```
-  9. ```car name: stringa contenente il nome dell'auto```
-
-
-### Contenuto dataset aggiornato
-  1. ```kpl: consumo in chilometri per litro```
-  2. ```cylinders: numero di cilindri ```
-  3. ```displacement: cilindrata in cc```
-  4. ```horsepower: potenza in cavalli```
-  5.```weight: peso in kg)```
-  6. ```acceleration: 0-100kph espressa in secondi```
-  7. ```model year: anno di produzione(dal 1970 al 2021)```
   8. ```origin: area di origine (1- USA, 2-Europa, 3-Asia)```
   9. ```car name: stringa contenente il nome dell'auto```
 
--- test.py
+++ test.py
@@ -1,11 +1,59 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.metrics import mean_squared_error
+
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import ElasticNet
+from sklearn.neighbors import KNeighborsRegressor
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 df = pd.read_csv('data/auto-mpg.csv')
 
-print(df.head())
-print(len(df))
-print(df.describe())
+pd.set_option('display.max_columns', None) # mostra tutte le colonne
 
-#Exploratory data Analysis visualization and analysis
+print(df.head())
+print(df.dtypes)
+
+# Exploratory data Analysis visualization and analysis
+plt.figure(figsize=(10, 8))
+sns.histplot(df.mpg)
+plt.show()
+
+# searching for null values
+print(df.isnull().sum())
+
+
+# Correlation
+#f, ax = plt.subplots(figsize=[14, 8])
+sns.heatmap(df.corr(), annot=True, fmt=".2f")
+#ax.set_title("Correlation Matrix", fontsize=20)
+plt.show()
+
+sns.pairplot(df, diag_kind='kde')
+plt.show()
+
+#plt.figure(figsize=[14, 6])
+#sns.barplot(x=df['model year'] + 1900, y=df['mpg'])
+#plt.title('Consumption Gallon by Years')
+#plt.show()
+
+print(df.corr('spearman'))
+
+# train test split
+x = df.drop(["mpg"], axis=1)
+y = df.mpg
+print(x.head())
+
+test_size = 0.3
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=test_size, random_state=42)
+
